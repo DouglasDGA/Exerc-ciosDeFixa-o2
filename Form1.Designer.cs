7843e465bcabe425caf0f167680d78070e909db4
DouglasDGA <douglas.dgalbuquerqu@senacsp.edu.br>
2023-02-16 19:41:23 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace ExercíciosDeFixação2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.BtnInserir = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Média de itens";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Média dos Valores";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(162, 46);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 290);
+            this.listBox2.TabIndex = 12;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(36, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 290);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // BtnInserir
+            // 
+            this.BtnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInserir.Location = new System.Drawing.Point(76, 342);
+            this.BtnInserir.Name = "BtnInserir";
+            this.BtnInserir.Size = new System.Drawing.Size(174, 47);
+            this.BtnInserir.TabIndex = 10;
+            this.BtnInserir.Text = "Inserir Dados";
+            this.BtnInserir.UseVisualStyleBackColor = true;
+            this.BtnInserir.Click += new System.EventHandler(this.BtnInserir_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(424, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 34);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Média de itens que são para compra com CNPJ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(563, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Média do valor de todos os itens em estoque";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(424, 46);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(133, 290);
+            this.listBox3.TabIndex = 15;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(563, 46);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(133, 290);
+            this.listBox4.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Nome do Produto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Valor do Produto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BtnInserir);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button BtnInserir;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercíciosDeFixação2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source = OSA0625215W10 - 1; Initial Catalog = ExercicioFixação; Integrated Security = True");
+        SqlCommand comando = new SqlCommand();
+        SqlDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comando.Connection = conn;
+            CarregarLista();
+        }
+        private void CarregarLista()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+
+            conn.Open();
+            comando.CommandText = "select * from Exercicio2";
+            dr = comando.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    listBox1.Items.Add(dr[1].ToString());
+                    listBox2.Items.Add(dr[2].ToString());
+                    listBox3.Items.Add(dr[3].ToString());
+                    listBox4.Items.Add(dr[4].ToString());
+                }
+            }
+            conn.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox l = sender as ListBox;
+            if (l.SelectedIndex != -1)
+            {
+                listBox1.SelectedIndex = l.SelectedIndex;
+                listBox2.SelectedIndex = l.SelectedIndex;
+                listBox3.SelectedIndex = l.SelectedIndex;
+                listBox4.SelectedIndex = l.SelectedIndex;
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox l = sender as ListBox;
+            if (l.SelectedIndex != -1)
+            {
+                listBox1.SelectedIndex = l.SelectedIndex;
+                listBox2.SelectedIndex = l.SelectedIndex;
+                listBox3.SelectedIndex = l.SelectedIndex;
+                listBox4.SelectedIndex = l.SelectedIndex;
+            }
+        }
+
+        private void BtnInserir_Click(object sender, EventArgs e)
+        {
+            if (BtnInserir.Text == "Inserir Dados")
+            {
+                limparControles();
+                if (novoArquivo.ShowDialog() != DialogResult.OK)
+                    return;
+
+                if (!novoArquivo.FileName.Contains(".csv"))
+                    return;
+                string caminho = novoArquivo.FileName;
+                string textoLido;
+
+                try
+                {
+                    textoLido = File.ReadAllText(caminho);
+                }
+                catch (Exception Exc)
+                {
+                    tbPrincipal.SelectedIndex = 1;
+                    lblErro.Text = Exc.Message;
+                    return;
+                }
+
+                int i = 0;
+                lboPessoas.Items.Clear();
+                foreach (var linha in textoLido.Split('\n'))
+                {
+                    if (linha == "" || linha == "\r") break;
+                    if (i != 0)
+                    {
+                        string[] tratamento = linha.Split(';');
+                        Pessoa ps = new Pessoa(tratamento[0], tratamento[1], DateTime.Parse(tratamento[2]), tratamento[3], tratamento[4]);
+                        lboPessoas.Items.Add(ps);
+                    }
+                    i++;
+                }
+                BtnInserir.Text = "Salvar Arquivo";
+            }
+
+            else
+            {
+                string txt = "Nome,Sobrenome,Data de Nascimento;Telefone;Email\n";
+                foreach (Pessoa pessoa in lboPessoas.Items)
+                {
+                    txt += pessoa.Nome + ";";
+                    txt += pessoa.Sobrenome + ";";
+                    txt += pessoa.DataNascimento + ";";
+                    txt += pessoa.Telefone + ";";
+                    txt += pessoa.Email + "\n";
+                }
+                txt = txt.Substring(0, txt.Length - 5);
+                salvarNovoArquivo.Filter = "Arquivo CSV|*.csv";
+                salvarNovoArquivo.Title = "Salvar Arquivo";
+                if (salvarNovoArquivo.ShowDialog() != DialogResult.OK && salvarNovoArquivo.FileName == null) return;
+                if (salvarNovoArquivo.ShowDialog() != DialogResult.Cancel) return;
+
+                try
+                {
+                    FileStream abrirArquivo = (FileStream)salvarNovoArquivo.OpenFile();
+                    StreamWriter salvandoArquivo = new StreamWriter(abrirArquivo);
+
+                    salvandoArquivo.WriteLine(txt);
+                    salvandoArquivo.Close();
+                    abrirArquivo.Close();
+                    BtnInserir.Text = "Inserir Dados";
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+        }
+    }
+}
